bf3611b96409dad94c4d2941b6779d682f3af940
levanhaikb <83411229+levanhaikb@users.noreply.github.com>
2025-06-01 16:30:31 +0700

    Create config.template.js

---
+++ config.template.js
@@ -0,0 +1,18 @@
+// Rename to config.js and fill in your values
+module.exports = {
+    discord: {
+        token: 'MTM3ODI5NTc3NzUxNTgwMjYyNA.G1u1Cn.ZR-6BauDbbdwPqjadvXgyDQfMB1E4C7YecUJOQ',
+        clientId: '1378295777515802624',
+        guildId: '1378299739354343424'
+    },
+    api: {
+        vpsApiUrl: 'http://localhost:3000',
+        webhookPort: 3002
+    },
+    channels: {
+        signalsChannelName: 'vietforex-signals'
+    },
+    logging: {
+        logFile: './logs/bot.log'
+    }
+};
